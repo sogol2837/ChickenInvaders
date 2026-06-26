287f894a1bf06e59bb9d1d293e701a9ccfdd70ee
sogol <sogolborjloo@gmail.com>
2026-06-26 12:30:43 +0330

    GameObject created

--- chickenInvaders/src/chickenInvaders/entity/GameObject.java
+++ chickenInvaders/src/chickenInvaders/entity/GameObject.java
@@ -1,4 +1,42 @@
 package chickenInvaders.entity;
 
-public class GameObject {
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+
+    public GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void update();
+
+    public abstract void draw(Graphics g);
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
